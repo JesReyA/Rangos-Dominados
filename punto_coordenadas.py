d976640f224b59da4824f9525d62b3d28f854e18
JesReyA <jesreyav@gmail.com>
2026-09-19 01:39:39 -0600

    Se agrega clase para crear objetos con coordenadas para un futuro, además se agrega gitignore

---
+++ punto_coordenadas.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 19 01:30:46 2026
+
+@author: j
+"""
+
+class PuntoCoordernadas:
+    def __init__(self, coordenadax, coordenaday):
+        self.coordenadax = coordenadax
+        self.coordenaday = coordenaday
+    
+    def __repr__(self):
+        return f"{self.coordenadax}, {self.coordenaday}"
