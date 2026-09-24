975900e744aa75efeafecd5a07d342be5c2d1da6
JesReyA <jesreyav@gmail.com>
2026-09-24 00:19:12 -0600

    Mejoras en constructor

--- nodo_arbol.py
+++ nodo_arbol.py
@@ -7,11 +7,11 @@ Created on Wed Sep 23 22:59:25 2026
 """
 
 class Nodo:
-    def __init__(self, coordenada_y_punto, punto_referenciado, altura = 1, hijo_izquierdo= None, hijo_derecho = None):
-        self.hijo_izquierdo = hijo_izquierdo                   #Puntero al hijo del nodo
-        self.hijo_derecho = hijo_derecho                       #Puntero al hijo del nodo
+    def __init__(self, coordenaday_punto, punto_referenciado):
+        self.hijo_izquierdo = None                   #Puntero al hijo del nodo
+        self.hijo_derecho = None                      #Puntero al hijo del nodo
         
-        self.coordenada_y_punto = coordenada_y_punto           #Coordenada guardada de cada punto que se compara
+        self.coordenaday_punto = coordenaday_punto           #Coordenada guardada de cada punto que se compara
         self.punto_referenciado = punto_referenciado          #Referencia al punto completo del cual pertenece la coordenada, servirá para mencionar exactamente a cuales se domina
-        self.altura = altura                                  #Se usará al momento del balanceo del arbol, además de en la parte de sweep line
+        self.altura = 1                                       #Se usará al momento del balanceo del arbol, además de en la parte de sweep line
         
