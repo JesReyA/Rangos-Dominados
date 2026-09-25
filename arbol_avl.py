079ae16152730ca0c16d22145b4d13d219890727
JesReyA <jesreyav@gmail.com>
2026-09-24 21:56:18 -0600

    Rotación izquierda terminada

--- arbol_avl.py
+++ arbol_avl.py
@@ -16,3 +16,21 @@ class ArbolAVL:
     
     def factor_balance(self, nodo):
         return 0  if (nodo) == None else self.obtener_altura(nodo.hijo_izquierdo) - self.obtener_altura(nodo.hijo_derecho)
+    
+    
+    def rotacion_izquierda(self, nodo):
+        nuevo_nodo_raiz = nodo.hijo_derecho             #Variable para guardar el nodo que subirá
+        
+        nodo_intercambiable_guardado = nuevo_nodo_raiz.hijo_izquierdo  #Variable necesaria para guardar el nodo de forma temporal
+    
+        nuevo_nodo_raiz.hijo_izquierdo = nodo           #El hijo izquierdo del nodo raíz ahora es todo todo lo que esta a la izquierda del raiz anterior, incluido este
+        
+        nodo.hijo_derecho = nodo_intercambiable_guardado        #El hijo derecho del subarbol izquierdo es el nodo que se guardó para no perderse
+        
+        #Se calculan las alturas de los subarboles modificados y se suma uno por el nodo actual que se toma en cuenta
+        nodo.altura = 1 + max(self.obtener_altura(nodo.hijo_izquierdo),self.obtener_altura(nodo.hijo_derecho))
+        nuevo_nodo_raiz.altura = 1 + max(self.obtener_altura(nuevo_nodo_raiz.hijo_izquierdo),self.obtener_altura(nuevo_nodo_raiz.hijo_derecho))
+        
+        #Se retorna el nodo que se ha modificado o subido para ser el nuevo
+        return nuevo_nodo_raiz
+        
